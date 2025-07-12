2b13c00be5f690a45ccf364ae9c05bdc12ee4665
ustoyarvi <129869048+ustoyarvi@users.noreply.github.com>
2025-07-12 22:11:34 +0400

    Update megaphone.cpp UNBEARABLE

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        std::cout << "* LOUD AND UNREADABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     }
     else
     {
